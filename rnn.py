7a9beac8533f84de67fb5f5e9f5e86c202d45adc
lucas-swiniarski <lucas.swiniarski@gmail.com>
2017-04-19 23:01:46 -0400

    RNN changes

--- rnn.py
+++ rnn.py
@@ -35,25 +35,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--dataroot', default='./data/', type=str, help='path to dataset')
 parser.add_argument('--batchSize', type=int, default=20, help='input batch size')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
-parser.add_argument('--layers', type=int, help='number of hidden layers', default=2)
-parser.add_argument('--nhidden', type=int, help='hidden layers size', default=100)
 parser.add_argument('--cuda'  , action='store_true', help='enables cuda')
 parser.add_argument('--epochs', type=int, default=25, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
-parser.add_argument('--balance', action='store_true', help='Balance Training set by sampling more minority class')
 parser.add_argument('--weights', action='store_true', help='Weighted Cross Entropy')
-parser.add_argument('--dropout', type=float, default=.5, help='Dropouts on discriminator')
-parser.add_argument('--noise', type=float, default=.1, help='Add gaussian noise to real data')
+parser.add_argument('--dropout', type=float, default=.0, help='Dropouts on discriminator')
+parser.add_argument('--noise', type=float, default=.0, help='Add gaussian noise to real data')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 ###
 # RNN args
 ###
 parser.add_argument('--model', type=str, default='LSTM', help='type of recurrent net (RNN_TANH, RNN_RELU, LSTM, GRU)')
 parser.add_argument('--optimizer', type=str, default='base', help='base | adam | RMSprop')
-parser.add_argument('--nhid', type=int, default=50,
-                    help='humber of hidden units per layer')
-parser.add_argument('--nlayers', type=int, default=1,
-                    help='number of layers')
+parser.add_argument('--layers', type=int, help='number of hidden layers', default=2)
+parser.add_argument('--nhidden', type=int, help='hidden layers size', default=100)
 parser.add_argument('--bptt', type=int, default=20,
                     help='sequence length')
 parser.add_argument('--clip', type=float, default=0.5,
@@ -61,7 +56,7 @@ parser.add_argument('--clip', type=float, default=0.5,
 args = parser.parse_args()
 
 inputSize = 217
-log_interval = 1000
+log_interval = 100
 eval_batch_size = 10
 
 def loaderize(data_X, data_Y, balance, batch_size):
@@ -73,7 +68,7 @@ def loaderize(data_X, data_Y, balance, batch_size):
 # Load Data in tensors
 ###
 
-trainloader = loaderize(np.load(args.dataroot+'train_X.pkl.npy'), np.load(args.dataroot+'train_y.pkl.npy'), args.balance, args.batchSize * args.bptt)
+trainloader = loaderize(np.load(args.dataroot+'train_X.pkl.npy'), np.load(args.dataroot+'train_y.pkl.npy'), False, args.batchSize * args.bptt)
 valloader = loaderize(np.load(args.dataroot+'val_X.pkl.npy'), np.load(args.dataroot+'val_y.pkl.npy'), False, eval_batch_size * args.bptt)
 testloader = loaderize(np.load(args.dataroot+'test_X.pkl.npy'), np.load(args.dataroot+'test_y.pkl.npy'), False, eval_batch_size * args.bptt)
 
@@ -93,9 +88,12 @@ class RNNModel(nn.Module):
         self.rnn_type = rnn_type
         self.nhid = nhid
         self.nlayers = nlayers
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, input, hidden):
+        input = self.dropout(input)
         output, hidden = self.rnn(input, hidden)
+        output = self.dropout(output)
         return self.classifier(output.view(output.size(0)*output.size(1), output.size(2))), hidden
 
     def init_hidden(self, bsz):
@@ -122,7 +120,7 @@ def repackage_hidden(h):
     else:
         return tuple(repackage_hidden(v) for v in h)
 
-model = RNNModel(args.model, inputSize, args.nhidden, args.nlayers, args.dropout)
+model = RNNModel(args.model, inputSize, args.nhidden, args.layers, args.dropout)
 
 input = torch.FloatTensor(args.bptt, args.batchSize, inputSize)
 label = torch.LongTensor(args.batchSize * args.bptt)
@@ -147,6 +145,8 @@ def train(trainloader, epoch):
     for i, (data, target) in enumerate(trainloader, 0):
         if data.size(0) < args.batchSize * args.bptt:
             smaller_batch_size= data.size(0) // args.bptt
+            if smaller_batch_size == 0:
+                break
             data = data[:smaller_batch_size * args.bptt]
             hidden = model.init_hidden(smaller_batch_size)
             data.resize_(args.bptt, smaller_batch_size, inputSize)
@@ -197,6 +197,8 @@ def test(testloader, epoch, isVal):
     for i, (data, target) in enumerate(testloader, 0):
         if data.size(0) < args.batchSize * args.bptt:
             smaller_batch_size= data.size(0) // args.bptt
+            if smaller_batch_size == 0:
+                break
             data = data[:smaller_batch_size * args.bptt]
             hidden = model.init_hidden(smaller_batch_size)
             data.resize_(args.bptt, smaller_batch_size, inputSize)
@@ -250,7 +252,7 @@ for epoch in range(1, args.epochs + 1):
     train(trainloader, epoch)
     val_loss = test(valloader, epoch, True)
 
-    if prev_val_loss and (val_loss > prev_val_loss):
+    if prev_val_loss and (val_loss > prev_val_loss) and (epoch % 5 == 0):
         lr /= 4.0
         print('|||lr change||| %.4f' % (lr))
     prev_val_loss = val_loss
